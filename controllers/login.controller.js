d6d71a1d7647b8caecbb24aebba0d5473de65684
Lucas Paviotti <lucas.paviotti@elpixel.com>
2021-11-23 23:00:53 -0300

    first commit

---
+++ controllers/login.controller.js
@@ -0,0 +1,22 @@
+const getLogin = async (req, res) => {
+    if (req.isAuthenticated()) {
+        console.log(req.isAuthenticated());
+        res.redirect('/')
+    } else {
+        res.render('login-facebook');
+    }
+}
+
+const postLogin = async (req, res) => {
+    res.redirect('/');
+}
+
+const failedLogin = async (req, res) => {
+    res.render('login-error');
+}
+
+module.exports = {
+    getLogin,
+    postLogin,
+    failedLogin
+}
